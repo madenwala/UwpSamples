b8930a5b2c05d689dd4c4464ac399439038df9c5
Mohammed Adenwala <adenwala@outlook.com>
2016-04-29 11:46:28 -0500

    Initial check-in

---
+++ AdaptiveLists/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptiveLists
+{
+    public class ListItem
+    {
+        public int Index { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+        public override string ToString()
+        {
+            return this.Description;
+        }
+    }
+
+    //public class ItemContainerStyleSelector : StyleSelector
+    //{
+    //    public Style OddItemStyle { get; set; }
+    //    public Style EventItemStyle { get; set; }
+    //    protected override Style SelectStyleCore(object item, DependencyObject container)
+    //    {
+    //        return ((ListItem)item).Index % 2 == 0 ? this.EventItemStyle : this.OddItemStyle;
+    //    }
+    //}
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            list.ItemsSource = Enumerable.Range(0, 100).Select(i => new ListItem
+            {
+                Index = i,
+                ImageURL = "Assets/Water.jpg",
+                Description = $"Item {i}"
+            }).ToList();
+
+            //ImageURL = "http://3.bp.blogspot.com/-fyjvPBm_INs/ThpaZFszL5I/AAAAAAAAAFw/6sJBTUj905c/s1600/water_drop.jpg",
+        }
+    }
+}
