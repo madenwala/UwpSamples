d7554561d031d2d2115a0901f4adb8e6f8a5b068
Mohammed Adenwala <madenwal@microsoft.com>
2016-05-11 13:20:54 +0200

    Initial check in

---
+++ XboxOneTitleSafeLists/Behaviors/GameControllerListFocusBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Contoso.Behaviors
+{
+    public class GameControllerListFocusBehavior : Behavior<Control>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            var list = sender as ListView;
+            if (list?.Items != null && list.Items.Count >= 0)
+            {
+                if (list.SelectedIndex == 0 && e.Key == VirtualKey.Left)
+                {
+                    e.Handled = true;
+                }
+                else if (list.SelectedIndex + 1 == list.Items.Count && e.Key == VirtualKey.Right)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
---
+++ XboxOneTitleSafeLists/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Contoso
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListView_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            var list = sender as ListView;
+            if (list?.Items != null && list.Items.Count >= 0)
+            {
+                if (list.SelectedIndex == 0 && e.Key == VirtualKey.Left)
+                {
+                    e.Handled = true;
+                }
+                else if (list.SelectedIndex + 1 == list.Items.Count && e.Key == VirtualKey.Right)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
